7bdf50706ab637d43f45278e0db4a072d72aafca
Josue Abraham <josuea@noukod.com>
2018-11-09 17:10:30 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,19 +21,19 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal_project FROM projects
+"SELECT projects.title, (projects.funding_goal - SUM(pledges.amount)) * -1 AS over_goal FROM projects
     INNER JOIN pledges
       ON projects.id = pledges.project_id
     GROUP BY projects.title
-    HAVING over_goal_project > 0;"
+    HAVING over_goal > 0;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"SELECT users.name, SUM(pledges.amount) AS sum_pledges FROM users
+"SELECT users.name, SUM(pledges.amount) AS total_pledges FROM users
     INNER JOIN pledges
       ON users.id = pledges.user_id
     GROUP BY users.name
-    ORDER BY sum_pledges;"
+    ORDER BY total_pledges;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
@@ -44,7 +44,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"SELECT projects.category, SUM(pledges.amount) AS sum_pledges FROM projects
+"SELECT projects.category, SUM(pledges.amount) AS total_pledges FROM projects
     INNER JOIN pledges
       ON projects.id = pledges.project_id
     WHERE category = 'books';"
